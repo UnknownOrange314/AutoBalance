847becf3a3cf1a92639dea7dd9a9f0552035e11a
Bharat <bponnaluri@gmail.com>
2014-01-03 16:35:34 -0500

    Choices now have target success rate

--- src/Choice.scala
+++ src/Choice.scala
@@ -1,4 +1,11 @@
-class Choice(val choiceName:String) {
+/**
+ *
+ * @param choiceName The name of the choice.
+ * @param targetUseRate How often do we want a choice to be used.
+ */
+class Choice(val choiceName:String,val targetUseRate:Double) {
+
+
 
     var successRate:Double=1.0
     /**
@@ -6,6 +13,9 @@ class Choice(val choiceName:String) {
      * -Track how often a player picks a certain choice and how well a player does.
      *  successRate=(how often player picks choice)*(how successful a player is).
      */
-    var popularity:Double=1.0  //Is this necessary.
+    var popularity:Double=1.0
+
     var power:Double=1.0
+
+
 }
